e8b415f872486b1f33a3f7f0542c736d4bb8975a
Michaela Olausson <michaelaolausson@gmail.com>
2020-03-22 23:19:24 +0100

    ...

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -22,11 +22,9 @@ const Auth = () => {
         try {
             const response = await tmdb.getRequestToken();
             setError(null)
-            if (window.location === "movie-monster-michaela.herokuapp.com" ) {
-                window.location.href = `${BASE_URL_TMDB}${response.data.request_token}?redirect_to=https://${APP_HOST}/approved`;
-            } else {
-                window.location.href = `https://www.themoviedb.org/authenticate/${response.data.request_token}?redirect_to=http://5.57.244.204:3000/approved`;
-            }    
+            window.location.href = `${BASE_URL_TMDB}${response.data.request_token}?redirect_to=https://${APP_HOST}/approved`;
+            //window.location.href = `https://www.themoviedb.org/authenticate/${response.data.request_token}?redirect_to=http://5.57.244.204:3000/approved`;
+          
         } catch {
             setError('Something went wrong logging you in. Sorry!')
         }
